4e22ffda391cbeb89041dfce064eba82260f4e71
Abhishek Krishna <abhishekkrishna123@outlook.com>
2018-07-12 01:40:04 +0530

    Changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RV Placement Alerts' });
 });
 
 module.exports = router;
